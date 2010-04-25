d44acc4437dae0bf8136aaf6ea88663bcc7ca976
Evan Broder <broder@mit.edu>
2010-04-25 07:25:58 +0000

    In printing-config:   * Add a debathena.printing Python module for common logic.
    Note that this involves dropping our use of
    DEB_AUTO_UPDATE_DEBIAN_CONTROL for this package.
    
    Generating the debian/control file involves figuring out which Python
    development packages need to be depended on, which involves parsing
    information out of the debian/control file. Catch-22. So the
    python-distutils class doesn't work with
    DEB_AUTO_UPDATE_DEBIAN_CONTROL.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+try:
+    from debian_bundle.changelog import Changelog
+    from debian_bundle.deb822 import Deb822
+    from email.utils import parseaddr
+
+    version = Changelog(open(path.join(path.dirname(__file__), 'debian/changelog')).read()).\
+              get_version().full_version
+
+    maintainer_full = Deb822(open(path.join(path.dirname(__file__), 'debian/control')))['Maintainer']
+    maintainer, maintainer_email = parseaddr(maintainer_full)
+except:
+    version = '0.0.0'
+    maintainer = ''
+    maintainer_email = ''
+
+setup(
+    name="debathena.printing",
+    version=version,
+    description="Printing configuration for Debathena.",
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    license="MIT",
+    packages=find_packages(),
+)
