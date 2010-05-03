a18f0e48904ab7bf4b8649a9d611971c1b2cf0a6
Evan Broder <broder@mit.edu>
2010-05-03 17:13:03 +0000

    Finish coverage of debathena.printing.find_queue in unit tests.

--- debathena/test_printing.py
+++ debathena/test_printing.py
@@ -14,18 +14,13 @@ class TestFindQueue(mox.MoxTestBase):
     def setUp(self):
         super(TestFindQueue, self).setUp()
 
-        def _setup_side_effects():
-            printing.CUPS_BACKENDS = ['get-print.mit.edu']
-        self.mox.StubOutWithMock(printing, '_setup')
-        printing._setup().WithSideEffects(_setup_side_effects)
-
-        self.mox.StubOutWithMock(printing, 'get_cups_uri')
+        self.mox.StubOutWithMock(printing, 'canonicalize_queue')
         self.mox.StubOutWithMock(printing, 'get_hesiod_print_server')
         self.mox.StubOutWithMock(printing, 'is_cups_server')
 
     def test_local_mdns_queue(self):
         """Verify that find_queue doesn't interfere with truly local printers."""
-        printing.get_cups_uri('foo').AndReturn('dnssd://patience._printer._tcp.local.')
+        printing.canonicalize_queue('foo').AndReturn(None)
 
         self.mox.ReplayAll()
 
@@ -34,7 +29,7 @@ class TestFindQueue(mox.MoxTestBase):
 
     def test_athena_cups_queue(self):
         """Verify that find_queue can find non-local Athena queues on CUPS"""
-        printing.get_cups_uri('ajax').AndReturn(None)
+        printing.canonicalize_queue('ajax').AndReturn('ajax')
         printing.get_hesiod_print_server('ajax').AndReturn('GET-PRINT.MIT.EDU')
         printing.is_cups_server('GET-PRINT.MIT.EDU').AndReturn(True)
 
@@ -45,7 +40,7 @@ class TestFindQueue(mox.MoxTestBase):
 
     def test_athena_lprng_queue(self):
         """Verify that find_queue can find non-local Athena queues on LPRng"""
-        printing.get_cups_uri('ashdown').AndReturn(None)
+        printing.canonicalize_queue('ashdown').AndReturn('ashdown')
         printing.get_hesiod_print_server('ashdown').AndReturn('MULCH.MIT.EDU')
         printing.is_cups_server('MULCH.MIT.EDU').AndReturn(False)
 
@@ -53,6 +48,28 @@ class TestFindQueue(mox.MoxTestBase):
 
         self.assertEqual(printing.find_queue('ashdown'),
                          (printing.SYSTEM_LPRNG, 'MULCH.MIT.EDU', 'ashdown'))
+
+    def test_misnamed_local_queue(self):
+        """Verify that find_queue will use canonicalized queue names"""
+        printing.canonicalize_queue('w20').AndReturn('ajax')
+        printing.get_hesiod_print_server('ajax').AndReturn('GET-PRINT.MIT.EDU')
+        printing.is_cups_server('GET-PRINT.MIT.EDU').AndReturn(True)
+
+        self.mox.ReplayAll()
+
+        self.assertEqual(printing.find_queue('w20'),
+                         (printing.SYSTEM_CUPS, 'GET-PRINT.MIT.EDU', 'ajax'))
+
+    def test_queue_with_instance(self):
+        """Verify that find_queue will strip instances"""
+        printing.canonicalize_queue('ajax/2sided').AndReturn('ajax/2sided')
+        printing.get_hesiod_print_server('ajax').AndReturn('GET-PRINT.MIT.EDU')
+        printing.is_cups_server('GET-PRINT.MIT.EDU').AndReturn(True)
+
+        self.mox.ReplayAll()
+
+        self.assertEqual(printing.find_queue('ajax/2sided'),
+                         (printing.SYSTEM_CUPS, 'GET-PRINT.MIT.EDU', 'ajax'))
 
 
 if __name__ == '__main__':
